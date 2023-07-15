1a4bd7dac53dc92186faba2167131fe90201a97d
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-15 21:36:59 +0900

    add sidebar Components about layout dir

---
+++ src/layouts/sidebar/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// css import
+import styles from "./Sidebar.module.css";
+
+
+const Sidebar: React.FC = () => {
+  return (
+    <>
+      <div className={styles.sidebar}>
+        sidebar
+      </div>
+    </>
+  )
+}
+
+export default Sidebar;
