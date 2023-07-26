652d30615e69f5970cf18d5c708cfe3dd4afc85a
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-27 07:01:15 +0900

    remove not use files and add atomic design

---
+++ src/app/components/templates/MainLayout.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+
+// css import
+import styles from "./styles/MainLayout.module.css";
+import Header from "../organisms/Header";
+
+
+const MainLayout: React.FC = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default MainLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Header from "@/layouts/header/Header";
-import styles from "../styles/page.module.css";
-import MainLayout from '@/layouts/Main/MainLayout';
-import Sidebar from "@/layouts/sidebar/Sidebar";
+import MainPage from "./pages";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <Header />
-        <Sidebar />
-        <MainLayout />
+    <main>
+      <MainPage />
     </main>
   )
 }
