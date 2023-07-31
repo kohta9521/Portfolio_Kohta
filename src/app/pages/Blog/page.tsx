70f0f094d905048c80877b384c95f20d3216f3be
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-31 21:16:41 +0900

    add pae routing setting but 404 error

---
+++ src/app/pages/Blog/page.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+import styles from "./BlogPage.module.css";
+
+
+const BlogPage: React.FC = () => {
+  return (
+    <div>
+      blog page
+    </div>
+  )
+}
+
+export default BlogPage;
