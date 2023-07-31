a1eb22a6281f07e5f0a3c6f1ff5a337dc80d635f
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-31 21:20:34 +0900

    add hero background-color

---
+++ src/app/components/organisms/Hero.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+// css import
+import styles from "./styles/Hero.module.css";
+
+
+const Hero: React.FC = () => {
+  return (
+    <div className={styles.hero}>
+hero
+    </div>
+  )
+}
+
+export default Hero;
