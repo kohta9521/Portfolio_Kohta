0c53782c2770496fcb2a88e1a7123132d29f4cbd
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-13 08:54:26 +0900

    title atom components not found so change branch #27

---
+++ src/app/components/organisms/Blog.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+// css import
+import styles from "./styles/Blog.module.css";
+
+
+const Blog: React.FC = () => {
+  return (
+    <div className={styles.blog}>
+            < id={0} idtext="blog" text="Blog" align="center" />
+    </div>
+  )
+}
+
+export default Blog;
