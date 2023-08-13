b316b6e69fe859c1efd0edba97af956c364e8c3f
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-13 09:17:43 +0900

    complet auto scroll about widtharrow but not end scroll

--- src/app/components/organisms/Blog.tsx
+++ src/app/components/organisms/Blog.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { FC, useRef, useEffect } from "react";
 
 // css import
@@ -25,9 +27,26 @@ const Blog: React.FC = () => {
   }, []);
   return (
     <div className={styles.blog}>
-            <Title id={0} idtext="blog" text="Blog" align="center" />
-            blog area
-            横スクロール実装
+      <Title id={0} idtext="blog" text="Blog" align="center" />
+      <div ref={screenRef} className={styles.screen}>
+        <div className={styles.container}>
+          <div className={styles.page}>
+            <div className={styles.text}>
+              page 1
+            </div>
+          </div>
+          <div className={styles.page}>
+            <div className={styles.text}>
+              page 2
+            </div>
+          </div>
+          <div className={styles.page}>
+            <div className={styles.text}>
+              page 3
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
