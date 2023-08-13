2714e314a120bafa69af7e78ee775408644cf45e
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-13 09:24:27 +0900

    add blogcard about child components remove

---
+++ src/app/components/atoms/BlogCard.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+// css import
+import styles from "./styles/BlogCard.module.css";
+
+
+const BlogCard: React.FC = () => {
+  return (
+    <div className={styles.blog}>
+
+    </div>
+  )
+}
+
+export default BlogCard;
