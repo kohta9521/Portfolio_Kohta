0e699a620285ee1e5e02dcfddb906e16be7a8d67
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-13 09:05:01 +0900

    add logic about blog wide scroll

--- src/app/components/organisms/Blog.tsx
+++ src/app/components/organisms/Blog.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useEffect } from "react";
 
 // css import
 import styles from "./styles/Blog.module.css";
@@ -6,6 +6,23 @@ import Title from "../atoms/Title";
 
 
 const Blog: React.FC = () => {
+  const screenRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    screenRef.current!.onwheel = ev  => {
+      ev.preventDefault();
+
+      let delta = (ev.deltaY / Math.abs(ev.deltaY)) * window.innerWidth;
+      if (delta > 0) {
+        delta += screenRef.current!.scrollLeft;
+        delta = Math.floor(delta / window.innerWidth) * window.innerWidth;
+      }else {
+        delta += screenRef.current!.scrollLeft;
+        delta = Math.ceil(delta / window.innerWidth) * window.innerWidth;
+      }
+      screenRef.current!.scrollLeft = delta;
+    }
+  }, []);
   return (
     <div className={styles.blog}>
             <Title id={0} idtext="blog" text="Blog" align="center" />
