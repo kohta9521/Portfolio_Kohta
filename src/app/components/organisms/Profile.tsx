20c0e802af6291025d654e6ea559a5811daffb32
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-16 23:45:15 +0900

    add profile relative desing

--- src/app/components/organisms/Profile.tsx
+++ src/app/components/organisms/Profile.tsx
@@ -12,13 +12,19 @@ const Profile: React.FC = () => {
     <div className={styles.profile}>
       <Title id={0} idtext="profile" text="My Profile" align="left" />
       <div className={styles.flexBox}>
-        <div className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A quibusdam sequi
-          quisquam necessitatibus sint sed optio adipisci ipsam. Dicta beatae mollitia 
-          erum officiis corrupti porro id ex at optio?
+        <div className={styles.textBox}>
+          <p className={styles.text}>
+          Nice to meet you, and thank you for visiting kohta engineer portfolio site.<br />
+          I am a second-year student at a university in Tokyo. I am a member of the law department of the Faculty of Law, and I am improving my technical skills through internships and books in order to become an engineer in the future.
+          Currently I am working mainly as a front-end engineer while also doing freelance work.<br />
+          I also run ENTER, an engineering club for university students only. Recently, I have been studying web 3.0 and other fields of interest.<br />
+          I hope you enjoy your time here!
+          </p>
         </div>
-        <img className={styles.flexImg} src="" alt="" />
-        <img className={styles.flexImg} src="" alt="" />
+        <div className={styles.imgFlexBox}>
+          <img className={styles.flexImg1} src="/img/profile2.jpg" alt="profileImg" />
+          <img className={styles.flexImg2} src="/img/profile.jpg" alt="profileImg" />
+        </div>
       </div>
     </div>
   )
