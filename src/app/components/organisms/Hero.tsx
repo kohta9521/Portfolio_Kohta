495d8065c5030956092ce43704acb11232c41a83
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-10 18:04:10 +0900

    add hero componets pc layouts about hero img

---
+++ src/app/components/organisms/Hero.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+
+// css import
+import styles from "./styles/Hero.module.css";
+import HeroTitleBox from '../molecules/HeroTitleBox';
+import Image from 'next/image';
+
+// components import
+
+
+
+const Hero: React.FC = () => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.container}>
+        <HeroTitleBox />
+        <div className={styles.imgBox}>
+          <img className={styles.img} src="/img/hero.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
--- src/app/components/templates/MainLayout.tsx
+++ src/app/components/templates/MainLayout.tsx
@@ -3,12 +3,14 @@ import React, { FC } from "react";
 // css import
 import styles from "./styles/MainLayout.module.css";
 import Header from "../organisms/Header";
+import Hero from "../organisms/Hero";
 
 
 const MainLayout: React.FC = () => {
   return (
     <>
     <Header />
+    <Hero />
     </>
   )
 }
