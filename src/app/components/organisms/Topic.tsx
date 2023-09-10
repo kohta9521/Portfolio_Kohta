918958b37ec867107ad4bf35f2bbcdc61209a38c
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-10 18:20:27 +0900

    add topi components

---
+++ src/app/components/organisms/Topic.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+
+// css import
+import styles from './styles/Topic.module.css';
+
+// comopnents import
+
+
+
+const Topic: React.FC = () => {
+  return (
+    <div className={styles.topic}>
+      <div className={styles.container}>
+
+      </div>
+    </div>
+  )
+}
+
+export default Topic;
--- src/app/components/templates/MainLayout.tsx
+++ src/app/components/templates/MainLayout.tsx
@@ -4,6 +4,7 @@ import React, { FC } from "react";
 import styles from "./styles/MainLayout.module.css";
 import Header from "../organisms/Header";
 import Hero from "../organisms/Hero";
+import Topic from "../organisms/Topic";
 
 
 const MainLayout: React.FC = () => {
@@ -11,6 +12,7 @@ const MainLayout: React.FC = () => {
     <>
     <Header />
     <Hero />
+    <Topic />
     </>
   )
 }
