4d67eaacbd2d1575d67c0b9f404e179dd9e55732
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-12 11:15:41 +0900

    add about resuponsive desing

--- src/app/components/organisms/About.tsx
+++ src/app/components/organisms/About.tsx
@@ -2,11 +2,11 @@ import React, { FC} from 'react';
 
 // css import
 import styles from "./styles/About.module.css";
-import SectionTitle from '../atoms/SectionTitle';
-import Button from '../atoms/Button';
+
 
 // components import
-
+import SectionTitle from '../atoms/SectionTitle';
+import Button from '../atoms/Button';
 
 
 const About: React.FC = () => {
@@ -15,7 +15,21 @@ const About: React.FC = () => {
       <div className={styles.container}>
         <SectionTitle id={1} fill={true} text='ABOUT' align='left' />
         <SectionTitle id={2} fill={false} text='THIS PORTFOLIO SITE' align='left' />
-        {/* <Button id={1} toLink='/' text={1} /> */}
+      <div className={styles.flexbox}>
+        <div className={styles.textbox}>
+          <p className={styles.text}>
+            Nice to meet you, and thank you for visiting kohta engineer portfolio site.<br />
+            I am a second-year student at a university in Tokyo. I am a member of the law department of the Faculty of Law, and I am improving my technical skills through internships and books in order to become an engineer in the future.
+            Currently I am working mainly as a front-end engineer while also doing freelance work.<br />
+            I also run ENTER, an engineering club for university students only. Recently, I have been studying web 3.0 and other fields of interest.
+            I hope you enjoy your time here!
+          </p>
+          <Button id={1} toLink='/' text={1}  />
+        </div>
+        <div className={styles.imgbox}>
+          <img className={styles.img} src="/img/hero.jpg" alt="aboutImg" />
+        </div>
+      </div>
       </div>
     </div>
   )
