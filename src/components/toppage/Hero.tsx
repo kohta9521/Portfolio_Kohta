d23c2809a53ee05b1d11f5f4102c6eb575f999f7
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-24 16:00:36 +0900

    add hero smarphone styling

--- src/components/toppage/Hero.tsx
+++ src/components/toppage/Hero.tsx
@@ -9,10 +9,10 @@ const Hero = () => {
     <div className={styles.hero}>
       <div className={styles.container}>
         <h1 className={styles.heroTitle}>
-          Kohta s Software Engineering Portfolio Site
+          Kohta Software Engineering Portfolio Site
         </h1>
         <p className={styles.heroSubTitle}>
-          On this site, you can view Kohta s productions and daily studies as an
+          On this site, you can view Kohta productions and daily studies as an
           engineer. Kohta is studying every day in order to work as a software
           engineer in the future!
         </p>
