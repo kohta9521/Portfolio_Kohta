afb9ffd1a43baab6e49509998f48d5f3b2f3fcb2
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-24 18:38:49 +0900

    add projects tags props completegit statusgit status

--- src/components/toppage/ProjectsCard.tsx
+++ src/components/toppage/ProjectsCard.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 // css import
 import styles from './styles/ProjectsCard.module.scss'
+
+// components impor
+import ProjectTags from './ProjectTags'
 
 // props type
 type Props = {
@@ -14,19 +18,21 @@ type Props = {
 
 const ProjectsCard = ({ id, imgLink, cardTitle, projectsTags }: Props) => {
   return (
-    <div className={styles.card} key={id}>
+    <Link href={`/projects/${id}`} className={styles.card} key={id}>
       <Image
         src={imgLink}
-        width={400}
-        height={350}
+        width={300}
+        height={280}
         quality={100}
         priority={true}
         alt="recentProjectsCardImg"
         className={styles.cardImg}
       />
       <p className={styles.cardTitle}>{cardTitle}</p>
-      <div className={styles.tagsFlexBox}>{projectsTags}</div>
-    </div>
+      <div className={styles.tagsFlexBox}>
+        <ProjectTags tagNumber={projectsTags} />
+      </div>
+    </Link>
   )
 }
 
