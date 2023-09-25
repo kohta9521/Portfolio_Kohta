45f0be4914b30a5b0323cf1721fd7a6985937f34
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-25 15:32:32 +0900

    add all files

---
+++ src/components/tagspage/PageHeroTemplate.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// css import
+import styles from './styles/PageHeroTempalte.module.scss'
+
+function PageHeroTemplate() {
+  return <div>temple</div>
+}
+
+export default PageHeroTemplate
