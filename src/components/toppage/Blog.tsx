356eaa8e68018bef09599dec82be89e2690345e4
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-27 20:47:01 +0900

    add new modal complete

--- src/components/toppage/Blog.tsx
+++ src/components/toppage/Blog.tsx
@@ -1,17 +1,17 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 
 // css import
 import styles from './styles/Blog.module.scss'
-import ModalCard from '../common/ModalCard'
+// import ModalCard from '../common/ModalCard'
+import NewModal from './NewModal'
 // import Modal from './Modal'
 
 const Blog = () => {
-  // const [show, setShow] = useState(false)
-
-  // const openModal = () => {
-  //   setShow(true)
-  // }
+  const [show, setShow] = useState(false)
+  const openModal = () => {
+    setShow(true)
+  }
 
   return (
     <>
@@ -19,42 +19,24 @@ const Blog = () => {
         <div className={styles.container}>
           <h1>===================================</h1>
           this areas modal is new modal child components
-          <ModalCard
+          <br />
+          <br />
+          <h1>========================</h1>
+          <h1>========================</h1>
+          <h1>========================</h1>
+          <button onClick={openModal}>Click</button>
+          <NewModal show={show} setShow={setShow} />
+          {/* <ModalCard
             key={1}
             imgLink="/images/sampleImg1.png"
             articleTitle="New Company Site!"
             articleDate="2023-09-26"
             tagsNumber={1}
-          />
-          <h1>===================================</h1>
-          <h1 className={styles.blogTitle}>BLOG</h1>
-          {/* <div>
-          <p>modal window sample</p>
-          <button onClick={openModal}>Click</button>
-          <Modal show={show} setShow={setShow} />
-        </div> */}
+          /> */}
         </div>
       </div>
     </>
   )
 }
-
-// function Modal(props: { show: boolean; setShow: any }) {
-//   const closeModal = () => {
-//     props.setShow(false)
-//   }
-//   if (props.show) {
-//     return (
-//       <div id="overlay" className={styles.overlay}>
-//         <div id="content" className={styles.content}>
-//           <p>this is modal window</p>
-//           <button onClick={closeModal}>Close</button>
-//         </div>
-//       </div>
-//     )
-//   } else {
-//     return null
-//   }
-// }
 
 export default Blog
