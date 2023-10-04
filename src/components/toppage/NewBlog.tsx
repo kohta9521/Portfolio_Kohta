9374456c1de67de373abe5da44ac55779155f5cd
Kohta9521 <kohta0421kochi@gmail.com>
2023-10-04 16:41:24 +0900

    add new section components bout NewBlog componnensts #93

---
+++ src/components/toppage/NewBlog.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// css import
+import styles from './styles/NewBlog.module.scss'
+
+// components import
+
+const NewBlog = () => {
+  return (
+    <div className={styles.newBlog}>
+      <div className={styles.container}>
+        new blog section (new nodal animation child components)
+      </div>
+    </div>
+  )
+}
+
+export default NewBlog
