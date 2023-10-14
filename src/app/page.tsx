dbb23889f19d0cb7a1142a527dd4f4bbcf028b7c
kohta <kohtakochi@kohtanoMacBook-Pro.local>
2023-10-14 23:29:56 +0900

    add new branch about storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
 import React from 'react'
-import Template from '@/components/layouts/Template1'
+
+
 import MouseStalker from '@/hooks/MouseStalker'
-import Hero from '@/components/toppage/Hero'
-import Project from '@/components/toppage/Project'
+
 
 // css import
 import styles from '../styles/page.module.css'
-import DropDown from '@/components/toppage/DropDown'
-import Profile from '@/components/toppage/Profile'
-import Blog from '@/components/toppage/Blog'
-import Blog2 from '@/components/toppage/Blog2'
-import NewBlog from '@/components/toppage/NewBlog'
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <MouseStalker />
-      <Template templateNumber={false}>
-        <Hero />
-        <Project />
-      </Template>
+      <h1>hi Storybook!</h1>
     </main>
   )
 }
