d5c6e4602342b251442b4c3176557343bb090a83
k_95.21 <kohta0421kochi@gmail.com>
2023-12-23 22:45:09 +0900

    add hambugermenu

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 
 
 // scss import
@@ -7,12 +8,54 @@ import Logo from '../atoms/Logo'
 
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+      };
     return (
-        <div className={styles.header}>
-            <div className={styles.container}>
-                <Logo id={1} link='/' size='medium' />
+        <header className={styles.header}>
+        <nav className={styles.nav}>
+          <div className={styles.logo}>
+            <a href="#">
+              <img src="logo.svg" alt="Your Company Logo" />
+            </a>
+          </div>
+  
+          <div className={`${styles.navLinks} ${isMenuOpen ? styles.show : ''}`}>
+            <div className={styles.dropdown}>
+              <button className={styles.dropbtn}>Product</button>
+              <div className={styles.dropdownContent}>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
             </div>
+            <a href="#">Features</a>
+            <a href="#">Marketplace</a>
+            <a href="#">Company</a>
+          </div>
+  
+          <div className={styles.login}>
+            <a href="#">Log in</a>
+          </div>
+  
+                {/* ハンバーガーメニューアイコン */}
+                <div className={styles.mobileMenuIcon} onClick={toggleMenu}>
+          {/* アイコンの表示切り替え */}
+          {isMenuOpen ? (
+            <img src="close.svg" alt="Close Menu" />
+          ) : (
+            <img src="menu.svg" alt="Open Menu" />
+          )}
         </div>
+        </nav>
+
+      {/* モバイルメニュー */}
+      <div className={`${styles.navLinks} ${isMenuOpen ? styles.show : ''}`}>
+        {/* メニュー項目 */}
+        {/* ... */}
+      </div>
+      </header>
     )
 }
 
