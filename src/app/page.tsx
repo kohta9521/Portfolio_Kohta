1500b97f4803b567888342e28212b82412450702
k_95.21 <kohta0421kochi@gmail.com>
2023-12-23 22:08:24 +0900

    add new mainlayout components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,17 @@ import styles from '../styles/page.module.css'
 
 // components import
 import MouseStalker from '@/hooks/MouseStalker'
+import MainLayout from '@/components/template/MainLayout'
+import { Main } from 'next/document'
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <MouseStalker />
+      <MainLayout id={1}>
+        <h1>sample</h1>
+      </MainLayout>
     </main>
   )
 }
---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+// scss import
+import styles from './styles/Header.module.scss'
+
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <div className={styles.container}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
