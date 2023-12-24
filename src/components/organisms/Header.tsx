3d54c97d79efbe5ca05328ff951cb58b0cd744a2
k_95.21 <kohta0421kochi@gmail.com>
2023-12-24 16:22:53 +0900

    add new storis

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,5 +1,4 @@
-'use client';
-import React, { useState } from 'react'
+import React from 'react'
 
 
 // scss import
@@ -7,55 +6,27 @@ import styles from './styles/Header.module.scss'
 import Logo from '../atoms/Logo'
 
 
+// components import
+
+
 const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-      };
     return (
         <header className={styles.header}>
-        <nav className={styles.nav}>
-          <div className={styles.logo}>
-            <a href="#">
-              <img src="logo.svg" alt="Your Company Logo" />
-            </a>
-          </div>
-  
-          <div className={`${styles.navLinks} ${isMenuOpen ? styles.show : ''}`}>
-            <div className={styles.dropdown}>
-              <button className={styles.dropbtn}>Product</button>
-              <div className={styles.dropdownContent}>
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-              </div>
-            </div>
-            <a href="#">Features</a>
-            <a href="#">Marketplace</a>
-            <a href="#">Company</a>
-          </div>
-  
-          <div className={styles.login}>
-            <a href="#">Log in</a>
-          </div>
-  
-                {/* ハンバーガーメニューアイコン */}
-                <div className={styles.mobileMenuIcon} onClick={toggleMenu}>
-          {/* アイコンの表示切り替え */}
-          {isMenuOpen ? (
-            <img src="close.svg" alt="Close Menu" />
-          ) : (
-            <img src="menu.svg" alt="Open Menu" />
-          )}
-        </div>
-        </nav>
+            <div className={styles.container}>
+                {/* logo area */}
+                <div className={styles.logoArea}>
+                    <Logo id={1} size='medium' link='/' />
+                </div>
+                {/* menu area */}
+                <div className={styles.menuArea}>
 
-      {/* モバイルメニュー */}
-      <div className={`${styles.navLinks} ${isMenuOpen ? styles.show : ''}`}>
-        {/* メニュー項目 */}
-        {/* ... */}
-      </div>
-      </header>
+                </div>
+                {/* contact area */}
+                <div className={styles.contactArea}>
+
+                </div>
+            </div>
+        </header>
     )
 }
 
