84cb2c45029010af21f22ff61695ceecbbf4adf7
k_95.21 <kohta0421kochi@gmail.com>
2024-02-01 13:49:42 +0900

    all files delete

--- frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    roots: ['<rootDir>/src'], // テストのルートディレクトリ
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest', // TypeScriptファイルの変換
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$', // テストファイルの検出
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // モジュールファイルの拡張子
-};
