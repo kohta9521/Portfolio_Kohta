68220b170c8b1aae26c09bf136cf11415a5efc4c
k_95.21 <kohta0421kochi@gmail.com>
2024-02-07 10:37:14 +0900

    add new sns link and screen top layout

--- frontend/components/organisms/HamburgerScreen.tsx
+++ frontend/components/organisms/HamburgerScreen.tsx
@@ -1,4 +1,12 @@
+import Link from 'next/link';
+import { FaInstagramSquare } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaSquareXTwitter } from 'react-icons/fa6';
+import { FaSquareGithub } from 'react-icons/fa6';
+
 import styles from './styles/HamburgerScreen.module.scss';
+
+// react icons
 
 export type HamburgerScreenProps = {
   isOpen: boolean;
@@ -9,9 +17,41 @@ const HamburgerScreen = ({ isOpen }: HamburgerScreenProps) => {
   return (
     <div className={`${styles.screen} ${isOpen ? styles.open : ''}`}>
       <div className={styles.leftBox}>
-        <h1>hello</h1>
+        <Link className={styles.hoverLink} key={1} href={'/'}>
+          <p className={styles.hoverTitle}>Download</p>
+          <p className={styles.hoverSubTitle}>資料ダウンロード</p>
+        </Link>
+        <Link className={styles.hoverLink} key={2} href={'/'}>
+          <p className={styles.hoverTitle}>Contact</p>
+          <p className={styles.hoverSubTitle}>各種お問い合わせ</p>
+        </Link>
       </div>
-      <div className={styles.rightBox}></div>
+      <div className={styles.centerBox}>
+        <div className={styles.container}></div>
+      </div>
+      <div className={styles.rightBox}>
+        <p className={styles.title}>Kohta Portfolio Site</p>
+        <p className={styles.address}>
+          〒150 - 0041
+          <br />
+          東京都渋谷区神南1-3-4 神南ビル 1F
+          <br />
+        </p>
+        <div className={styles.snsBox}>
+          <Link href={'/'} className={styles.snsLink}>
+            <FaSquareXTwitter />
+          </Link>
+          <Link href={'/'} className={styles.snsLink}>
+            <FaSquareGithub />
+          </Link>
+          <Link href={'/'} className={styles.snsLink}>
+            <FaInstagramSquare />
+          </Link>
+          <Link href={'/'} className={styles.snsLink}>
+            <FaLinkedin />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
