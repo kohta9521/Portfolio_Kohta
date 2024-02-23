25cbc2c7a3434c91b72cc9bad064b93ac1ccda5c
k_95.21 <kohta0421kochi@gmail.com>
2024-02-23 11:27:30 +0900

    add new sectiona bout footer and workd

---
+++ frontend/types/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    // 他のカスタムカラーをここに追加
+  },
+  // 他のテーマカスタマイズをここに追加
+});
+
+export default theme;
